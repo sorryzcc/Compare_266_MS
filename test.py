a8ee9ac7a83aec245e382fa960f3a4de6b248e1a
zcc <675623045@qq.com>
2024-10-18 18:24:34 +0800

    feat:1018日使用

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # 加载Excel文件
-df1 = pd.read_excel('266总表1016.xlsx')
-df2 = pd.read_excel('266总表1017.xlsx')
+df1 = pd.read_excel('266总表1017.xlsx')
+df2 = pd.read_excel('266总表1018.xlsx')
 
 # 检查列名，确保它们是一致的
 print(df1.columns)
@@ -20,4 +20,4 @@ merged_df = df1.merge(df2, on='Key', how='outer', suffixes=('_1', '_2'))
 diff_df = merged_df[(merged_df['Translate_1'] != merged_df['Translate_2'])]
 
 # 输出不同的记录到新的Excel文件
-diff_df.to_excel('266总表1016与266总表1017.xlsx', index=False)
+diff_df.to_excel('266总表1017与266总表1018.xlsx', index=False)
